8461060d9aff5918ea7b98392181e3725a985aaa
Sunaina <sunainakumar926@gmail.com>
2026-01-31 10:03:44 +0530

    Initial commit with failing test

---
+++ test_api.py
@@ -0,0 +1,9 @@
+﻿"""Test file with missing import - will fail CI"""
+
+def test_github_api():
+    # Missing import: requests module not imported
+    response = requests.get('https://api.github.com')
+    assert response.status_code == 200
+
+def test_simple():
+    assert 1 + 1 == 2
